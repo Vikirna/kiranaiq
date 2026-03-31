78d6397428697fc24d5eacb519b3348cdec3b8e1
Vikirna <vikirna.03@gmail.com>
2026-03-31 20:02:30 +0530

    fix CORS for vercel

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001'] }))
+app.use(cors({
+    origin: [
+      'http://localhost:3000',
+      'https://kiranaiq-kappa.vercel.app'
+    ],
+    credentials: true
+  }))
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/authRoutes'))
