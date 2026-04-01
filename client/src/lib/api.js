39ab1f5372ddc4ce99023436af73d76052a35b2d
Vikirna <vikirna.03@gmail.com>
2026-04-01 10:09:22 +0530

    hardcode railway url directly

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://kiranaiq-production.up.railway.app/api'
+  baseURL: 'https://kiranaiq-production.up.railway.app/api'
 })
 
 API.interceptors.request.use((config) => {
